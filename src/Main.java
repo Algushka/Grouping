57f572d64f73abcde6921b365333ae54c2600749
ASUS <successolga118@gmail.com>
2025-05-15 15:04:59 +0300

    3 task

--- src/Main.java
+++ src/Main.java
@@ -28,16 +28,16 @@ public class Main {
         List <Task> t2 = List.of(new Task(4, "task1", "done", 8),
                 new Task(6, "task1", "done", 8),
                 new Task(5, "task1", "done", 8),
-                new Task(8, "task1", "done", 8),
+                new Task(8, "task1", "in process", 8),
                 new Task(9, "task1", "done", 8),
-                new Task(2, "task1", "done", 8),
+                new Task(2, "task1", "finished", 8),
                 new Task(11, "task1", "done", 8),
                 new Task(7, "task1", "done", 8)
         );
         List <Task> t3 = List.of(new Task(4, "task1", "done", 8),
-                new Task(6, "task1", "done", 8),
+                new Task(6, "task1", "in process", 8),
                 new Task(5, "task1", "done", 8),
-                new Task(8, "task1", "done", 8),
+                new Task(8, "task1", "finished", 8),
                 new Task(9, "task1", "done", 8)
         );
 
@@ -62,13 +62,17 @@ public class Main {
 
         List <List<Task>> listOfTasks = new ArrayList(List.of(t1,t2,t3));
         Map<Integer, List<Task>> allTasksGroupingByNumbers = listOfTasks.stream()
-                .flatMap(s -> s.stream()).collect(Collectors.groupingBy(s -> s.getNumber()))
+                .flatMap(s -> s.stream()).collect(Collectors.groupingBy(s -> s.getNumber()));
         System.out.println(allTasksGroupingByNumbers);//map из ключика TaskNumber и самой задачи
         //конец второй задачи
 
         //третья задача
-
-
+//        Дан список Programmer(String name, String city, List<Task>  tasks).
+//                Каждый программист  имеет список задач
+//        Task (int Number, String description, String status, int daysInProcessing) .
+//        Сформировать Map<String, List<Task>> где ключ, это статус задачи, а значение список задач в этом статусе
+        Map<String, List<Task>> allTasksGroupingByStatus = listOfTasks.stream().flatMap(s -> s.stream()).collect(Collectors.groupingBy(s -> s.getStatus()));
+        System.out.println(allTasksGroupingByStatus );
 
     }
 }
